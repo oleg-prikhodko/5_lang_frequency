8e51599a893f5f523b0fc412d00026665a298cb7
Oleg <super.oleg.3000@yandex.ru>
2018-09-21 17:02:38 +0300

    Name fixes

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from pprint import pprint
 
 
-def load_data(filepath):
+def load_text(filepath):
     with open(filepath) as text_file:
         text = text_file.read()
         return text
@@ -13,12 +13,12 @@ def load_data(filepath):
 def get_most_frequent_words(text):
     text = re.sub(r"\W", " ", text)
     words = text.lower().split()
-    word_count = defaultdict(lambda: 0)
+    frequency_by_word = defaultdict(lambda: 0)
     for word in words:
-        word_count[word] += 1
+        frequency_by_word[word] += 1
 
     top_ten_frequent_words = sorted(
-        word_count.items(), key=lambda item: item[1], reverse=True
+        frequency_by_word.items(), key=lambda item: item[1], reverse=True
     )[:10]
     return top_ten_frequent_words
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     filepath = sys.argv[1]
     try:
-        text = load_data(filepath)
+        text = load_text(filepath)
         top_ten_frequent_words = get_most_frequent_words(text)
         pprint(top_ten_frequent_words)
     except FileNotFoundError:
