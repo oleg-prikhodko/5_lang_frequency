ba62e97767029dff6d7bed957d99646e5057dbec
Oleg <super.oleg.3000@yandex.ru>
2018-09-21 16:08:11 +0300

    Initial commit

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,36 @@
+import re
+import sys
+from collections import defaultdict
+from pprint import pprint
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as text_file:
+        text = text_file.read()
+        return text
 
 
 def get_most_frequent_words(text):
-    pass
+    text = re.sub(r"\W", " ", text)
+    words = text.lower().split()
+    word_count = defaultdict(lambda: 0)
+    for word in words:
+        word_count[word] += 1
+
+    top_ten_frequent_words = sorted(
+        word_count.items(), key=lambda item: item[1], reverse=True
+    )[:10]
+    return top_ten_frequent_words
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("No filepath argument")
+
+    filepath = sys.argv[1]
+    try:
+        text = load_data(filepath)
+        top_ten_frequent_words = get_most_frequent_words(text)
+        pprint(top_ten_frequent_words)
+    except FileNotFoundError:
+        sys.exit("File not found")
